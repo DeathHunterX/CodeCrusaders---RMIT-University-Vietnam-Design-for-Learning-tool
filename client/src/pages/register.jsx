88acf915d2dcc86442a7be9ecbd03533a66b48ba
phanngan811 <101790003+phanN8111@users.noreply.github.com>
2023-06-14 22:45:15 +0700

    Add UI login/register

---
+++ client/src/pages/register.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignUp from "../components/Login/Register/SignUp";
+
+const RegisterPage = () => {
+  return (
+    <>
+      <SignUp />
+    </>
+  );
+};
+
+export default RegisterPage;

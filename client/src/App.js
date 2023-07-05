7432d608d28ca465eb0fe83375d9bc1ad7464cc6
phanngan811 <101790003+phanN8111@users.noreply.github.com>
2023-07-05 09:44:54 +0700

    view and download PDF file

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from './components/Navbar/navBar';
-import HomePage from './components/Home/home';
+import NavBar from "./components/Navbar/navBar";
+import HomePage from "./components/Home/home";
 import PageRender from "./customRouter/PageRender";
+
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import DownloadPDF from "./pages/downloadPDF";
+import { PDFDownloadLink } from "@react-pdf/renderer";
+import PDFFile from "./components/DownloadPDF/PDFFile";
+import { PDFViewer } from "@react-pdf/renderer";
+import Download from "./components/DownloadPDF/Download";
 
 // import Login from "./pages/login";
 // import RegisterPage from "./pages/register";
@@ -14,26 +22,40 @@ function App() {
   }
 
   return (
-      <Router>
-        <div className="App">
-          <div className='main'>
-              <NavBar sideBarStats={sideBarStats} changeState={handleState}/>
-              
-              <section id="main_screen" className={`${sideBarStats === true ? 'nav_is_closed' : ''}`}>
-                  <Routes>
-                    <Route exact path='/' element={<HomePage />} />
-                    <Route exact path='/'>
-                      <Route exact path='/:page' element={<PageRender />} />
-                      <Route exact path='/:page/:id' element={<PageRender />} />
-                      <Route exact path='/:page/:id/:subPage' element={<PageRender />} />
-                      <Route exact path='/:page/:id/:subPage/:subId' element={<PageRender />} />
-                    </Route>
+    <Router>
+      <div className="App">
+        <div className="main">
+          <NavBar sideBarStats={sideBarStats} changeState={handleState} />
 
-                  </Routes>
-              </section>
-          </div>
+          <section
+            id="main_screen"
+            className={`${sideBarStats === true ? "nav_is_closed" : ""}`}
+          >
+            <Routes>
+              <Route exact path="/" element={<HomePage />} />
+
+              <Route exact path="/down-preview" element={<Download />} />
+
+              <Route exact path="/pdf" element={<HomePage />} />
+              <Route exact path="/">
+                <Route exact path="/:page" element={<PageRender />} />
+                <Route exact path="/:page/:id" element={<PageRender />} />
+                <Route
+                  exact
+                  path="/:page/:id/:subPage"
+                  element={<PageRender />}
+                />
+                <Route
+                  exact
+                  path="/:page/:id/:subPage/:subId"
+                  element={<PageRender />}
+                />
+              </Route>
+            </Routes>
+          </section>
         </div>
-      </Router>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/pages/downloadPDF.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PDFFile from "../components/DownloadPDF/PDFFile";
+
+const DownloadPDF = () => {
+  return (
+    <>
+      <PDFFile />
+    </>
+  );
+};
+
+export default DownloadPDF;

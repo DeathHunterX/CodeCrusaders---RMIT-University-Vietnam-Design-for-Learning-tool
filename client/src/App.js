e37d4894e9c49d49133458ff356f77476e5ac56d
phanngan811 <101790003+phanN8111@users.noreply.github.com>
2023-07-25 09:12:37 +0700

    connect-Back-end

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,10 @@ import PrivateRouter from "./customRouter/PrivateRouter";
 import LoginPage from "./pages/login";
 import RegisterPage from "./pages/register";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import Layout from "./components/Layout";
 function App() {
-
   const [isNavBarOpen, setIsNavBarOpen] = useState(true);
-
 
   return (
     <Router>
@@ -23,24 +21,28 @@ function App() {
           <Layout isNavBarOpen={isNavBarOpen} setNavBarOpen={setIsNavBarOpen}>
             <Routes>
               {/* Authentication an Authorization */}
-              <Route exact path="/login" element={<LoginPage/>} />
-              <Route exact path="/register" element={<RegisterPage/>} />
-              
+              <Route exact path="/login" element={<LoginPage />} />
+              <Route exact path="/register" element={<RegisterPage />} />
+
               <Route exact path="/" element={<PrivateRouter />}>
-                
-                    <Route exact path="/" element={<HomePage />} />
+                <Route exact path="/" element={<HomePage />} />
 
-                    <Route exact path="/down-preview" element={<Download />} />
+                <Route exact path="/down-preview" element={<Download />} />
 
-                    
-                    <Route exact path="/:page" element={<PageRender />} />
-                    <Route exact path="/:page/:id" element={<PageRender />} />
-                    <Route exact path="/:page/:id/:subPage" element={<PageRender />} />
-                    <Route exact path="/:page/:id/:subPage/:subId" element={<PageRender />}/>
-                    
+                <Route exact path="/:page" element={<PageRender />} />
+                <Route exact path="/:page/:id" element={<PageRender />} />
+                <Route
+                  exact
+                  path="/:page/:id/:subPage"
+                  element={<PageRender />}
+                />
+                <Route
+                  exact
+                  path="/:page/:id/:subPage/:subId"
+                  element={<PageRender />}
+                />
               </Route>
             </Routes>
-
           </Layout>
         </div>
       </div>
@@ -56,7 +58,6 @@ function App() {
         pauseOnHover
         theme="light"
       />
-
     </Router>
   );
 }

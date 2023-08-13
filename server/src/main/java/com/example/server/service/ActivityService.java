f7557b3bed89913611a4ba84480ff6913c6c7486
khang <khangnick14@gmail.com>
2023-08-13 22:19:34 +0700

    feat: change logic of create activity to match FE

--- server/src/main/java/com/example/server/service/ActivityService.java
+++ server/src/main/java/com/example/server/service/ActivityService.java
@@ -3,6 +3,7 @@ package com.example.server.service;
 import com.example.server.api.request.ActivityRequest;
 import com.example.server.model.Activity;
 import com.example.server.model.Course;
+import com.example.server.model.Session;
 import com.example.server.model.activities.*;
 import org.springframework.http.ResponseEntity;
 
@@ -15,7 +16,7 @@ public interface ActivityService {
 
     Activity getActivityById(UUID id);
 
-    Activity createActivity(ActivityRequest activityRequest, Course course);
+    Activity createActivity(ActivityRequest activityRequest, Course course, Session session);
 
     Activity updateActivity(UUID activityId, ActivityRequest activityRequest);
 

3f4b2ef5d27584d89f0bb4554432d0f6ec99f697
khang <khangnick14@gmail.com>
2023-08-28 20:17:20 +0700

    feat: add route for getting course id and module id from share link

---
+++ server/src/main/java/com/example/server/api/response/SharedLinkResponse.java
@@ -0,0 +1,15 @@
+package com.example.server.api.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+public class SharedLinkResponse {
+  private UUID courseId;
+  private UUID moduleId;
+}
--- server/src/main/java/com/example/server/service/SharedCourseLinkService.java
+++ server/src/main/java/com/example/server/service/SharedCourseLinkService.java
@@ -1,6 +1,9 @@
 package com.example.server.service;
 
+import com.example.server.api.response.PDFResponse;
+import com.example.server.api.response.SharedLinkResponse;
 import com.example.server.model.SharedCourseLink;
+import org.springframework.http.ResponseEntity;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -9,6 +12,6 @@ public interface SharedCourseLinkService {
   SharedCourseLink saveShareLink(SharedCourseLink sharedCourseLink);
   SharedCourseLink findDetailsByShareLink(String shareLink);
   SharedCourseLink findShareLinkById(UUID linkId);
-
   SharedCourseLink generateLink(UUID moduleId);
+  ResponseEntity<?> getCourseModuleId(String shareLink);
 }
